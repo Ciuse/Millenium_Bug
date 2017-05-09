08a33ea79881519159967780e5d522549254ad5e
Ciuse <giuseppe3.mauri@mail.polimi.it>
2017-05-09 17:42:09 +0200

    prova commit da intellij e prime classi

---
+++ src/main/java/it/polimi/ingsw/ps31/Card.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.ps31;
+
+/**
+ * Created by Giuseppe on 09/05/2017.
+ */
+public abstract class Card {
+    private final static String name = null;
+
+    public String getName (){
+        return this.name;
+    }
+
+
+}
---
+++ src/main/java/it/polimi/ingsw/ps31/DevelopmentCard.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.ps31;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by Giuseppe on 09/05/2017.
+ */
+public abstract class DevelopmentCard extends Card {
+
+    private static final CardColor cardColor = null;
+    private static final Period period = null;
+    private static final Resource cost = null;
+
+
+
+
+}
