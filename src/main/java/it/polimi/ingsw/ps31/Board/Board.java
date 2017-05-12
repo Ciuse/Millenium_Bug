61ac61f0601beb57f240aefd44243d3f9a01b1fd
Francesco <franos.95@gmail.com>
2017-05-12 09:20:22 +0200

    abstract class Board added

--- src/main/java/it/polimi/ingsw/ps31/Board/Board.java
+++ src/main/java/it/polimi/ingsw/ps31/Board/Board.java
@@ -3,5 +3,7 @@ package it.polimi.ingsw.ps31.Board;
 /**
  * Created by Francesco on 12/05/2017.
  */
-public class Board {
+public abstract class Board {
+
+    /* Qui verranno inseriti i metodi per la visualizzazione grafica delle varie board nella GUI*/
 }
