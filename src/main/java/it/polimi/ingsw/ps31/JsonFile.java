542850949b5af02c9c09710f0d1ff1d9094974b0
Ciuse <giuseppe3.mauri@mail.polimi.it>
2017-05-13 17:18:03 +0200

    Add some class for json

--- src/main/java/it/polimi/ingsw/ps31/JsonFile.java
+++ src/main/java/it/polimi/ingsw/ps31/JsonFile.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.ps31;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
