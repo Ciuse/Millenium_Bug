d13b8490c655f75ae54a44c055d2293487ae9d84
Ciuse <giuseppe3.mauri@mail.polimi.it>
2017-06-05 11:41:40 +0200

    test

---
+++ src/main/java/it/polimi/ingsw/ps31/message/Messaggio1.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.ps31.message;
+
+import java.util.Observable;
+
+/**
+ * Created by Giuseppe on 02/06/2017.
+ */
+public class Messaggio1 extends MessageExample {
+    @Override
+    public void doSomething() {
+        System.out.println("ACCHIAPPATO");
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/ps31/message/Messaggio2.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.ps31.message;
+
+import java.util.Observable;
+
+/**
+ * Created by Giuseppe on 02/06/2017.
+ */
+public class Messaggio2 extends MessageExample {
+    @Override
+    public void doSomething() {
+        System.out.println("messaggio2");
+
+    }
+}
